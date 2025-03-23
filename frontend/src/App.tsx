603d995b544506ac69b64c318423aa78b2e1c017
Trevor McCasland <trevormccasland1@gmail.com>
2025-03-23 14:09:24 -0500

    feat: add select navigator

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,38 @@
-import { FC } from "react"
-import ActivityList from "./components/ActivityList"
-import ExerciseList from "./components/ExerciseList"
-import WorkoutList from "./components/WorkoutList"
-
+import React, { useState, FC } from 'react';
+import { Select, MenuItem, FormControl, InputLabel, SelectChangeEvent } from '@mui/material';
+import ActivityList from "./components/ActivityList"; 
+import ExerciseList from "./components/ExerciseList";
+import WorkoutList from "./components/WorkoutList";
 
 const App: FC = () => {
-    return <WorkoutList />
-}
+  const [selectedList, setSelectedList] = useState<string>('workout');
 
-export default App
+  const handleListChange = (event: SelectChangeEvent) => {
+    setSelectedList(event.target.value);
+  };
+
+  return (
+    <>
+      <FormControl fullWidth>
+        <InputLabel id="list-select-label">Select List</InputLabel>
+        <Select
+          labelId="list-select-label"
+          id="list-select"
+          value={selectedList}
+          label="Select List"
+          onChange={handleListChange}
+        >
+          <MenuItem value={'activity'}>Activity List</MenuItem>
+          <MenuItem value={'exercise'}>Exercise List</MenuItem>
+          <MenuItem value={'workout'}>Workout List</MenuItem>
+        </Select>
+      </FormControl>
+
+      {selectedList === 'activity' && <ActivityList />}
+      {selectedList === 'exercise' && <ExerciseList />}
+      {selectedList === 'workout' && <WorkoutList />}
+    </>
+  );
+};
+
+export default App;
--- frontend/src/components/ActivityList.tsx
+++ frontend/src/components/ActivityList.tsx
@@ -1,28 +1,58 @@
-import { List, ListItem, ListItemText, ListItemButton } from "@mui/material"
+import { List, ListItem, ListItemText, ListItemButton, ListItemIcon, Stack } from "@mui/material"
 import { useEffect, useState } from "react"
 import { Activity } from "../types"
 import activityServiceClient from "../services/activityServiceClient";
 import ActivityPage from "./ActivityPage";
+import { ModeEdit, Visibility } from "@mui/icons-material";
+import ActivityCard from "./ActivityCard";
 
 const ActivityList = () => {
     const [activitys, setActivitys] = useState<Activity[]>([]);
     const [selectedIndex, setSelectedIndex] = useState<number>(-1)
+    const [view, setView] = useState({
+        index: -1,
+        edit: false,
+        view: false
+    })
     useEffect(() => {
         const callGetActivitys = async () => {
             const data = await activityServiceClient.getActivities()
-            setActivitys(data.activities)
+            setActivitys(data)
         }
         callGetActivitys()
     }, [])
-    if (selectedIndex !== -1) {
-        return <ActivityPage activity={activitys[selectedIndex]} />
+    if (view.edit) {
+        return <ActivityPage activity={activitys[view.index]} />
+    }
+    if (view.view) {
+        return <ActivityCard activity={activitys[view.index]} />
     }
     return <List>
         {activitys.map((activity, i) => (
             <ListItem key={activity.name + i}>
-                <ListItemButton onClick={() => setSelectedIndex(i)}>
-                    <ListItemText primary={activity.name} />
-                </ListItemButton>
+                <ListItemText primary={activity.name} />
+                <Stack direction='row'>
+                    <ListItemButton  onClick={() => setView({
+                            index: i,
+                            view: false,
+                            edit: true
+                        })}
+                    >
+                        <ListItemIcon>
+                            <ModeEdit />
+                        </ListItemIcon>
+                    </ListItemButton>
+                    <ListItemButton onClick={() => setView({
+                            index: i,
+                            view: true,
+                            edit: false
+                        })}
+                    >
+                        <ListItemIcon>
+                            <Visibility />
+                        </ListItemIcon>
+                    </ListItemButton>
+                </Stack>
             </ListItem>
         ))}
     </List>
