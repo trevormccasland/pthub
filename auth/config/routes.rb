22e1063d3ebb0bb91664ee73a156e22e21be3d4f
Trevor McCasland <trevormccasland1@gmail.com>
2025-07-04 15:39:50 -0500

    update favicon

--- auth/config/routes.rb
+++ auth/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#google_auth'
+  get '/favicon.ico', to: redirect(ActionController::Base.helpers.asset_path('favicon.ico'))
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
 end
